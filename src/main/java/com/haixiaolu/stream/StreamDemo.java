8d5c3188b4042dfd73a8778646e16c5c1ee78ffb
haixiaolu <haixiao.lu-1@ou.edu>
2022-11-01 22:48:49 -0500

    added stream流的获取和中间操作方法

--- src/main/java/com/haixiaolu/stream/StreamDemo.java
+++ src/main/java/com/haixiaolu/stream/StreamDemo.java
@@ -1,22 +1,150 @@
 package com.haixiaolu.stream;
 
+import lombok.val;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamDemo {
 
     public static void main(String[] args) {
-        List<Author> authors = getAuthors();
-        System.out.println(authors);
+        // 查看基本信息
 
+//        List<Author> authors = getAuthors();
+//        System.out.println(authors);
+
+//        打印所有年龄小于18的作家的名字， 并且要注意去重
+
+        // lambda 写法
+        List<Author> authors = getAuthors();
+        authors.stream() // 把集合转换成流
+                .distinct() // 先去除重复的作家
+                .filter(author -> author.getAge() < 18) // 筛选年龄小于18的
+                .forEach(author -> System.out.println(author.getName())); // 遍历打印名字
+
+        // 第一种写法
+//        List<Author> authors1 = getAuthors();
+//        authors1.stream()
+//                .distinct()
+//                .filter(new Predicate<Author>() {
+//                    @Override
+//                    public boolean test(Author author) {
+//                        return author.getAge() < 18;
+//                    }
+//                })
+//                .forEach(new Consumer<Author>() {
+//                    @Override
+//                    public void accept(Author author) {
+//                        System.out.println(author.getName());
+//                    }
+//                });
+
+        // 打印所有姓名长度大于1的作家的姓名
+        // filter
+        authors.stream()
+                .filter(author ->author.getName().length() > 1)
+                .forEach(author -> System.out.println(author.getName()));
+        // map
+        // 打印所有作家的名字
+        authors.stream()
+                .map(author -> author.getName())
+                .forEach(name -> System.out.println(name));
+
+        authors.stream()
+                .map(author -> author.getAge())
+                .map(age -> age + 10)
+                .forEach(age -> System.out.println(age));
+
+        // 对流中的元素按照年龄进行降序排序， 并且要求不能有重复的元素
+        authors.stream()
+                .distinct()
+                .sorted(new Comparator<Author>() {
+                    @Override
+                    public int compare(Author o1, Author o2) {
+                        return o2.getAge() - o1.getAge();
+                    }
+                })
+                .forEach(author -> System.out.println(author.getAge()));
+
+
+        // limit
+        // 需求：对流中的元素按照年龄进行降序排序， 并且要求不能有重复的元素
+        authors.stream()
+                .distinct()
+                .sorted((o1, o2) -> o2.getAge() - o1.getAge())
+                .limit(2)
+                .forEach(author -> System.out.println("author: " + author.getName()));
+
+        // skip
+        // 打印除了年龄最大的作家外的其他作家， 要求不能重复元素， 并且按照年龄降序排序
+        authors.stream()
+                .distinct()
+                .sorted(((o1, o2) -> o2.getAge() - o1.getAge()))
+                .skip(2)
+                .forEach(author -> System.out.println(author.getAge()));
+
+        // flatMap
+        // 打印所有书籍的名字， 要求对重复的元素进行去重
+        // 匿名类实现法
+        authors.stream()
+                .flatMap(new Function<Author, Stream<Book>>() {
+                    @Override
+                    public Stream<Book> apply(Author author) {
+                        return author.getBooks().stream();
+                    }
+                })
+                .distinct()
+                .forEach(new Consumer<Book>() {
+                    @Override
+                    public void accept(Book book) {
+                        System.out.println(book.getName());
+                    }
+                });
+
+        // lambda
+        authors.stream()
+                .flatMap((Function<Author, Stream<Book>>) author -> author.getBooks().stream())
+                .distinct()
+                .forEach(book -> System.out.println(book.getName()));
+
+        // 打印现有数据的所有分类， 要求对分类进行去重， 不能出现这种格式： 哲学，爱情
+
+        authors.stream()
+                .flatMap(author -> author.getBooks().stream())
+                .distinct()
+                .flatMap(book -> Arrays.stream(book.getCategory().split(",")))
+                .distinct()
+                .forEach(category -> System.out.println(category));
+
+        /**
+         * 终结操作
+         */
+        // forEach 的用法： 输出所有作家的名字
+        authors.stream()
+                .map(author -> author.getName())
+                .distinct()
+                .forEach(name -> System.out.println(name));
+
+        // 2. count: 打印这些作家的所出书籍的数目， 删除重复元素
+        long count1 = authors.stream()
+                .map(author -> author.getBooks().stream())// 书籍
+                .distinct()
+                .count();
+        System.out.println(count1);
     }
 
     private static List<Author> getAuthors(){
         Author author = new Author(1L, "蒙多", 33, "一个从菜刀中明悟这里的人", null);
         Author author2 = new Author(2L, "亚拉索", 15, "狂风也追逐不上他的思考速度", null);
         Author author3 = new Author(3L, "易", 14, "是这个世界在限制他的思维", null);
-        Author author4 = new Author(4L, "易", 14, "是这个时间在限制他的思维", null);
+        Author author4 = new Author(3L, "易", 14, "是这个时间在限制他的思维", null);
 
         List<Book> books1 = new ArrayList<>();
         List<Book> books2 = new ArrayList<>();
